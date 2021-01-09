7052b55acfb0420640c2044c3c2c58fd88526b46
Henrique <heoliveirac@gmail.com>
2021-01-09 17:31:52 -0300

    C++

---
+++ Beeginner/Exercise 1006/1006.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double A, B, C, MEDIA;
+    cout << fixed << setprecision(1);
+    cin >> A;
+    cin >> B;
+    cin >> C;
+
+    MEDIA = (A * 2.0 + B * 3.0 + C * 5.0) /10;
+    cout << "MEDIA = " << MEDIA  << endl;
+return 0;
+}
